20bf550b8caf3a76b888408471dbf7ef64bbd1aa
katsu105 <zgok105@yahoo.co.jp>
2021-07-19 22:28:51 +0900

    test

--- front-app/pages/index.js
+++ front-app/pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          <a>これはdockerで立ち上げたnext-app</a>
+          <a>これはGitHub Actionでpushしたnext-app</a>
         </h1>
 
         <p className="description">
